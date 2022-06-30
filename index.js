e5b99e06c1f0b08e63f1768e65eef8ab019ae71a
Paulo Monezi <paulo.monezi@icloud.com>
2022-06-30 15:19:28 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+document.getElementById('btn-confirm').addEventListener('click', () => {
+    var userActual = document.getElementById('input-actual').value * 12
+    var userOffer = document.getElementById('input-offer').value * 12
+    console.log(userActual)
+    console.log(userOffer)
+
+    document.querySelector('.user-actual').innerHTML = `Custo do sistema atual em 12 meses R$ ${userActual},00`
+    document.querySelector('.user-offer').innerHTML = `Custo do sistema Vale Up em 12 meses R$ ${userOffer},00. <span>Você terá uma economia de R$${userActual - userOffer},00</span>`
+
+})
