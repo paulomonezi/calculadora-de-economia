777cd0b2bf4c1cb748832edd34aa7032f1de4048
Paulo Monezi <paulo.monezi@icloud.com>
2022-07-01 10:36:45 -0400

    viewport mobile tests

--- index.js
+++ index.js
@@ -6,28 +6,18 @@ document.getElementById('btn-confirm').addEventListener('click', () => {
     var userDeploymentActual = document.getElementById('input-deployment-expenses').value
     var userDeploymentOffer = document.getElementById('input-deployment-costs').value
 
-    console.log(userActual)
-    console.log(userDeploymentActual)
-    var actualTotalCosts = parseInt(userActual) + parseInt(userDeploymentActual)
-    console.log(actualTotalCosts)
+    var actualTotalCosts = parseFloat(userActual) + parseFloat(userDeploymentActual)
 
-    console.log(userOffer)
-    console.log(userDeploymentOffer)
-    var offerTotalCosts = parseInt(userOffer) + parseInt(userDeploymentOffer)
+    var offerTotalCosts = parseFloat(userOffer) + parseFloat(userDeploymentOffer)
 
     if (userDeploymentActual == "0") {
         document.querySelector('.user-actual').innerHTML = `Custo do sistema atual em ${months} meses: R$ ${userActual},00`
         document.querySelector('.user-offer').innerHTML = `Custo do sistema Vale Up em ${months}: R$ ${offerTotalCosts},00. <br><span>Você terá uma economia de <strong>R$${userActual - offerTotalCosts},00</strong></span>`
-
     }
     else {
         document.querySelector('.user-actual').innerHTML = `Custo do sistema atual em ${months} meses: R$ ${actualTotalCosts},00`
         document.querySelector('.user-offer').innerHTML = `Custo do sistema Vale Up em ${months}: R$ ${offerTotalCosts},00. <br><span>Você terá uma economia de <strong>R$${actualTotalCosts - offerTotalCosts},00</strong></span>`
     }
-
-
-
-
 })
 
 document.getElementById('radio-yes').onclick = function () {
@@ -42,5 +32,6 @@ document.getElementById('radio-no').onclick = function () {
     const disabled = document.getElementById("input-deployment-expenses").disabled;
     if (disabled != true) {
         document.getElementById("input-deployment-expenses").disabled = true;
+        document.getElementById("input-deployment-expenses").value = 0
     }
 }
