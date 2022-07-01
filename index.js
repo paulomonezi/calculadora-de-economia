68f45743d27eef15b881e79e8677fc9b130cd3b2
Paulo Monezi <paulo.monezi@icloud.com>
2022-06-30 22:01:22 -0400

    aditions on form

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+var months = 12
+
 document.getElementById('btn-confirm').addEventListener('click', () => {
-    var userActual = document.getElementById('input-actual').value * 12
-    var userOffer = document.getElementById('input-offer').value * 12
+    var userActual = document.getElementById('input-actual').value * months
+    var userOffer = document.getElementById('input-offer').value * months
+    var userDeploymentOffer = document.getElementById('input-deployment-costs').value
     console.log(userActual)
     console.log(userOffer)
+    console.log(userDeploymentOffer)
 
-    document.querySelector('.user-actual').innerHTML = `Custo do sistema atual em 12 meses: R$ ${userActual},00`
-    document.querySelector('.user-offer').innerHTML = `Custo do sistema Vale Up em 12 meses: R$ ${userOffer},00. <br><span>Você terá uma economia de <strong>R$${userActual - userOffer},00</strong></span>`
+    document.querySelector('.user-actual').innerHTML = `Custo do sistema atual em ${months} meses: R$ ${userActual},00`
+    document.querySelector('.user-offer').innerHTML = `Custo do sistema Vale Up em ${months}: R$ ${userOffer},00. <br><span>Você terá uma economia de <strong>R$${userActual - userOffer},00</strong></span>`
 
 })
